aa599a2a82a45cc953e574793a34be2077b80f22
Vitovt <v.vitovt@gmail.com>
2024-01-23 10:42:47 +0000

    Fix checkout

--- public/create-checkout-session.php
+++ public/create-checkout-session.php
@@ -7,6 +7,24 @@ require_once 'shared.php';
 // flow.
 $domain_url = $_ENV['DOMAIN'];
 
+if( isset($_POST) )
+{
+     $OrderId = (int)$_POST['OrderId'];
+     $Amount = (int)$_POST['Amount1'] + (int)$_POST['Amount2'] * 100;
+     $Description = $_POST['Description'];
+}
+
+
+//$clint
+
+$price = $stripe->prices->create([
+  'currency' => 'eur',
+  'unit_amount' => $Amount,
+  'product_data' => ['name' => 'Payment for client number ' . $Description ],
+]);
+
+$price_id = $price->id;
+
 // Create new Checkout Session for the order
 // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
 $checkout_session = $stripe->checkout->sessions->create([
@@ -15,9 +33,48 @@ $checkout_session = $stripe->checkout->sessions->create([
   'mode' => 'payment',
   // 'automatic_tax' => ['enabled' => true],
   'line_items' => [[
-    'price' => $_ENV['PRICE'],
+    'price' => $price_id,
     'quantity' => 1,
-  ]]
+  ]],
+    'custom_text' => [
+        'submit' => [
+            'message' => 'Пополнение баланса диллера: ' . $Description,
+            // Custom message alongside the payment confirmation button
+        ],
+        'after_submit' => [
+            'message' => 'Спасибо за оплату!'
+            // Add your custom message here, up to 1200 characters
+        ],
+    ],
+    //'description' => 'Описание заказа',
+    'custom_fields' => [
+        [
+            'key' => 'dealer_id',
+            'label' => [
+                'custom' => 'Dealer ID',
+                'type' => 'custom'
+            ],
+            'type' => 'text',
+            'text' => [
+                'maximum_length' => 7, // Set your desired maximum length
+                'minimum_length' => 1   // Set your desired minimum length
+            ],
+            'optional' => false // Set to true if the field is not mandatory
+        ],
+        [
+            'key' => 'client_id',
+            'label' => [
+                'custom' => 'Client ID',
+                'type' => 'custom'
+            ],
+            'type' => 'text',
+            'text' => [
+                'maximum_length' => 20, // Set your desired maximum length
+                'minimum_length' => 1   // Set your desired minimum length
+            ],
+            'optional' => false // Set to true if the field is not mandatory
+        ]
+    ],
 ]);
 
 header("HTTP/1.1 303 See Other");
